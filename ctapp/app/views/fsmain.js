4bea48916151b38cc44f1e1a33225a7c3d592b00
Scott McNeal <scott.mcneal@gmail.com>
2016-02-15 04:32:37 +0000

    intermediate stable build

--- ctapp/app/views/fsmain.js
+++ ctapp/app/views/fsmain.js
@@ -9,6 +9,7 @@ app.controller('FSMainController', function($scope, FSUser) {
     if (this.verbose) console.log("FSMainController::init()");
     fsuser = FSUser.user;
     fsuser.mockdata();
+    fsuser.models[0].setupAccounts(fsuser.accounts);
   };
 
   $scope.getUserJSON = function() {
@@ -16,7 +17,23 @@ app.controller('FSMainController', function($scope, FSUser) {
   };
   
   $scope.generateSchedule = function() {
-    return fsuser.generateSchedule();
+    var _start = (new Date()).getTime()/1000.0;
+    
+    var _return = fsuser.generateSchedule();
+    
+    var _end = (new Date()).getTime()/1000.0;
+    
+    console.log('elapsed time for generate schedule() = ' + (_end-_start) + ' seconds');
+    
+    
+    
+    _end = (new Date()).getTime()/1000.0;
+    console.log('total time = '+ (_end-_start) + ' seconds')
+    
+    return _return;
   }
 
+  $scope.getLedger= function(account_id) {
+    return fsuser.models[0].getLedger(account_id);
+  }
 });
